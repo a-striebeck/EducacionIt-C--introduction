36fde0a7916d10f9eec7fc68389a34d79dcc5293
Agustin Striebeck <agustinstriebeck.it@gmail.com>
2024-02-23 20:15:03 -0300

    Stage 1

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ProyectoIntegrador;
+/*Realizar un programa que solicite al alumno 
+los siguientes datos:
+● Nombre.
+● Apellido.
+● Edad.
+● Correo electrónico.
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name;
+        string lastName;
+        int age;
+        string Email;
+        string errorMessage = "Datos incorrectos. Ejecutar nuevamente la aplicación";
+        char reply;
+        int careerCode;
+
+        Console.WriteLine("Ingrese Nombre:");
+        name = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Ingrese su Apellido:");
+        lastName = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("Ingrese su edad:");
+        age = int.Parse(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("Ingrese su correo electronico:");
+        Email = Console.ReadLine();
+
+        Console.WriteLine("--- Datos del alumno ---");
+        Console.WriteLine("Nombre: " + name);
+        Console.WriteLine("Apellido: "+ lastName);
+        Console.WriteLine("Edad: " + age);
+        Console.WriteLine("Email: " + Email);
+
+        Console.WriteLine("¿Los datos son correctos? - 'S' para confirmar 'N' para cancelar");
+        reply = char.Parse(Console.ReadLine());
+
+        if (reply == 's' || reply == 'S')
+        {
+            Console.WriteLine("Ingrese el codigo de la carrera: ");
+            Console.WriteLine("1. Programacion .NET");
+            Console.WriteLine("2. Programacion Java");
+            Console.WriteLine("3. Programacion PHP");
+            careerCode = int.Parse(Console.ReadLine());
+            if (careerCode == 1 || careerCode == 2|| careerCode == 3)
+            {
+                Console.WriteLine("--- Constancia de inscripción ---");
+                Console.WriteLine("Alumno: "+ name + " " + lastName );
+                Console.WriteLine("Correo Electronico: "+ Email);
+                switch (careerCode)
+                {
+                    case 1:
+                        Console.WriteLine("Carrera: Programacion .NET");
+                        break;
+                    case 2:
+                        Console.WriteLine("Carrera: Programacion Java");
+                        break;
+                    case 3:
+                        Console.WriteLine("Carrera: Programacion PHP");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine(errorMessage);
+            }
+            
+        }
+        else{
+            Console.WriteLine(errorMessage);
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # EducacionIt-C--introduction
+
 Educacion IT - Introduccion a C#
+
+Este repositorio contiene los avances en el trabajo integrador planteado por Educacion IT para demostrar y poner en practica los conocimientos vistos en el curso.
+
+El proyecto consiste en la creacion de un sistema de inscripciones de una institucion educativa.
+
+En esta primera instancia, se trabajan con los tipos primitivos y los condicionales "if" y "switch", a traves de una aplicacion de consola sencilla que permite al alumno ingresar sus datos, confirmar que sean correctos y elegir la carrera.
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+This repository contains the advances in the integrative work proposed by Educacion IT to demonstrate and put into practice the knowledge gained in the course.
+
+The objective of this project is to create a registration system for an educational institution.
+
+In this first instance, we use primitive types and "if" and "switch" conditionals in a simple console application that lets the student enter their data, check if they are correct, and choose a career.
